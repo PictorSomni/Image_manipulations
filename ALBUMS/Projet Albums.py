d69fad8272006d2e442c605ea9847afa65a6bede
Charles Dessenius <dessenius.charles@gmail.com>
2022-09-23 15:08:49 +0200

    Update Projet Albums.py
    Added terminal colors using the "rich" module.
    It doesn't change anything, it's just prettier.

--- ALBUMS/Projet Albums.py
+++ ALBUMS/Projet Albums.py
@@ -3,9 +3,15 @@
 #                          IMPORTS                          #
 #############################################################
 import os
+import sys
 from time import sleep
 from PIL import Image
+from rich import print
+from rich.console import Console
 
+#############################################################
+#                         VARIABLES                         #
+#############################################################
 PROJECT = False
 WATERMARK = False
 MAXSIZE = 640
@@ -20,30 +26,41 @@ os.chdir(PATH)
 #############################################################
 #                         CONTENT                           #
 #############################################################
+console = Console()
 EXTENSION = (".JPG", ".JPEG", ".PNG", "GIF")
 FOLDER = [file for file in sorted(os.listdir()) if file.upper().endswith(EXTENSION) and not file == "watermark.png"]
 WATERMARK = "C:\\Users\\charl\\Documents\\PYTHON\\Image manipulation\\watermark.png" # Or just "watermark.png" if you copy it to the current folder.
 TOTAL = len(FOLDER)
 
 #############################################################
+#                         FUNCTIONS                         #
+#############################################################
+## Clears the terminal
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+#############################################################
 #                           MAIN                            #
 #############################################################
-for i, file in enumerate(FOLDER):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(f"Conversion en {MAXSIZE}px + filigrane")
-    print("Image {} sur {}".format(i+1, TOTAL))
+clear()
+print(f"[deep_sky_blue1]Conversion en {MAXSIZE}px + filigrane[/deep_sky_blue1]")
+print("[violet]~[/violet]" * 32)
 
-    try:
-        base_image = Image.open(file)
-    except Exception:
-        continue
-    else:
-        base_image.thumbnail((MAXSIZE,MAXSIZE), Image.LANCZOS)
-        watermark = Image.open(WATERMARK)
-        base_image.paste(watermark, watermark)
-        base_image.convert("RGB").save(f"{PATH}\\{file}", format="JPEG", subsampling=0, quality=QUALITY)
+with console.status("[bold blue]En cours...") as status:
+    for i, file in enumerate(FOLDER):
+        try:
+            base_image = Image.open(file)
+        except Exception:
+            continue
+        else:
+            base_image.thumbnail((MAXSIZE,MAXSIZE), Image.LANCZOS)
+            watermark = Image.open(WATERMARK)
+            base_image.paste(watermark, watermark)
+            base_image.convert("RGB").save(f"{PATH}\\{file}", format="JPEG", subsampling=0, quality=QUALITY)
 
 
-print("Terminé !")
-sleep(1)
-# input("Terminé !\nAppuyez sur une touche pour fermer")
+print("[bright_green]Terminé ![/bright_green]")
+
+print("[deep_sky_blue1]Belle journée![/deep_sky_blue1]")
+sleep(.5)
+sys.exit(1)
