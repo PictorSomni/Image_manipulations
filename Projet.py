c0432acd9c8655c8261f49ccedef852155922e03
Charles Dessenius <dessenius.charles@gmail.com>
2022-09-29 16:50:32 +0200

    Update "projet.py"

--- Projet.py
+++ Projet.py
@@ -3,15 +3,13 @@
 #                          IMPORTS                          #
 #############################################################
 import os
-import sys
-import re
 from time import sleep
 from PIL import Image
 
 PROJECT = False
 WATERMARK = False
-MAXSIZE = 512
-QUALITY = 70
+MAXSIZE = 640
+QUALITY = 85
 
 #############################################################
 #                           PATH                            #
@@ -24,20 +22,8 @@ os.chdir(PATH)
 #############################################################
 EXTENSION = (".JPG", ".JPEG", ".PNG", "GIF")
 FOLDER = [file for file in sorted(os.listdir()) if file.upper().endswith(EXTENSION) and not file == "watermark.png"]
-WATERMARK = "C:\\Users\\charl\\Documents\\PYTHON\\Image manipulation\\watermark2.png" # Or just "watermark.png" if you copy it to the current folder.
+WATERMARK = "C:\\Users\\charl\\Documents\\PYTHON\\Image manipulation\\watermark.png" # Or just "watermark.png" if you copy it to the current folder.
 TOTAL = len(FOLDER)
-
-file_name = re.search(r"([\w\s]+).py", sys.argv[0])
-if "PROJET" in file_name.group(1).upper() :
-    PROJECT = True
-
-if "watermark.png" in os.listdir() :
-    WATERMARK = True
-
-
-def folder(folder) :
-    if not os.path.exists(PATH + f"\\{folder}") :
-        os.makedirs(PATH + f"\\{folder}")
 
 
 #############################################################
@@ -45,7 +31,7 @@ def folder(folder) :
 #############################################################
 for i, file in enumerate(FOLDER):
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(f"Conversion en {MAXSIZE}px + renommage ({file_name.group(1)}) + filigrane")
+    print(f"Conversion en {MAXSIZE}px + renommage + filigrane")
     print("Image {} sur {}".format(i+1, TOTAL))
 
     try:
@@ -53,17 +39,16 @@ for i, file in enumerate(FOLDER):
     except Exception:
         continue
     else:
-        base_image.thumbnail((MAXSIZE,MAXSIZE), Image.LANCZOS)
-        folder("projet")
+        base_image.thumbnail((MAXSIZE,MAXSIZE), Image.Resampling.LANCZOS)
 
-        if WATERMARK:
-            watermark = Image.open(WATERMARK)
-            base_image.paste(watermark, watermark)
-
-        if PROJECT :
-            base_image.convert("RGB").save(f"{PATH}\\projet\\{file}", format="JPEG", subsampling=0, quality=QUALITY)
-        else :
-            base_image.convert("RGB").save("{PATH}\\projet\\{file_name.group(1)}_{i:03}.jpg", format="JPEG", subsampling=0, quality=QUALITY)
+    try:
+        watermark = Image.open(WATERMARK)
+    except Exception:
+        watermark = Image.open("watermark.png")
+        continue
+    else :
+        base_image.paste(watermark, watermark)
+        base_image.convert("RGB").save(f"{PATH}\\projet_{file}", format="JPEG", subsampling=0, quality=QUALITY)
 
 print("Terminé !")
 sleep(1)
