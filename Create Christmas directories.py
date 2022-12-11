6f9c9af15ab4866e7c9526e2bbb4b7b32ef9166f
Charles Dessenius <dessenius.charles@gmail.com>
2022-12-11 07:43:08 +0100

    Create Create Christmas directories.py
    Creates the directories that i need for the photos organised for Christmas.
    This does creates them for me.

---
+++ Create Christmas directories.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#############################################################
+#                          IMPORTS                          #
+#############################################################
+import os
+import sys
+import re
+from time import sleep
+
+#############################################################
+#                           PATH                            #
+#############################################################
+PATH = os.path.dirname(os.path.abspath(__file__))
+os.chdir(PATH)
+
+#############################################################
+#                         CONTENT                           #
+#############################################################
+NAME = 9
+halves = False
+# how_many = re.search(r"Create (\d+) directories.py", sys.argv[0])
+# HOW_MANY = int(how_many.group(1))
+HOW_MANY = 16
+#############################################################
+#                           MAIN                            #
+#############################################################
+for index, directories in enumerate(range(1, HOW_MANY + 1)) :
+    if halves == False :
+        if not os.path.exists(f"{PATH}\\{NAME:02}00") :
+            os.makedirs(f"{PATH}\\{NAME:02}H00")
+            os.makedirs(f"{PATH}\\{NAME:02}H00\\RAW")
+            os.makedirs(f"{PATH}\\{NAME:02}H00\\RAW\\SELECTION")
+            os.makedirs(f"{PATH}\\{NAME:02}H00\\RAW\\AUTRES")
+    else :
+        if not os.path.exists(f"{PATH}\\{NAME:02}30") :
+            os.makedirs(f"{PATH}\\{NAME:02}H30")
+            os.makedirs(f"{PATH}\\{NAME:02}H30\\RAW")
+            os.makedirs(f"{PATH}\\{NAME:02}H30\\RAW\\SELECTION")
+            os.makedirs(f"{PATH}\\{NAME:02}H30\\RAW\\AUTRES")
+            NAME += 1
+    halves = not halves
+
+sleep(1)
+print("Terminé !")
+sleep(1)
+sys.exit(1)
