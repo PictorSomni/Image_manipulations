bf582f4c16bddcfd1fb9e312f511707f17b32385
Charles Dessenius <dessenius.charles@gmail.com>
2024-10-03 17:41:34 +0200

    New watermark and remerciements.py
    Added an ALPHA variable to the remerciements.py which allows to modify easily the opacity of the watermark when applied.

--- Remerciements.py
+++ Remerciements.py
@@ -15,6 +15,7 @@ HEIGHT = 102    # mm
 DPI = 300       # DPI
 MAXSIZE = 512
 QUALITY = 75
+ALPHA = 0.42
 
 #############################################################
 #                           PATH                            #
@@ -33,7 +34,7 @@ WATERMARK = "C:\\Users\\charl\\Documents\\PYTHON\\Image manipulation\\watermark.
 IMAGES = []
 REQUIRED = ["recto", "verso", "duo"]
 BIG = ["int", "ext"]
-FORBIDDEN = ["10x15", "projet"]
+FORBIDDEN = ["10x15", "13x18", "projet"]
 duo = True
 
 #############################################################
@@ -53,8 +54,10 @@ new_image = Image.new('RGB', (WIDTH_DPI * 2, HEIGHT_DPI))
 
 for file in FOLDER :
     file_name = re.search(r"([\w\s]+).\w+", file)
+
     if any(required_name in file_name.group(1).lower() for required_name in REQUIRED) == True and not any(forbidden_name in file_name.group(1).lower() for forbidden_name in FORBIDDEN) == True :
         IMAGES.append(file)
+
     elif any(big_name in file_name.group(1).lower() for big_name in BIG) == True and not any(forbidden_name in file_name.group(1).lower() for forbidden_name in FORBIDDEN) == True :
         duo = False
         IMAGES.append(file)
@@ -64,10 +67,21 @@ for image in IMAGES :
 
     project = base_image.copy()
     project.thumbnail((MAXSIZE,MAXSIZE))
-    watermark = Image.open(WATERMARK)
-    project.paste(watermark, watermark)
-    project.convert("RGB").save(f"{PATH}\\Projet_{image}", format="JPEG", subsampling=0, quality=QUALITY)
-    print(f"{image} : Projet OK")
+    try:
+        watermark = Image.open(WATERMARK)
+        if watermark.mode != "RGBA":
+            watermark = watermark.convert("RGBA")
+
+        r, g, b, a = watermark.split()
+        a = a.point(lambda i: i * ALPHA)
+        watermark = Image.merge("RGBA", (r, g, b, a))
+    except Exception:
+        watermark = Image.open("watermark.png")
+        continue
+    else :
+        project.paste(watermark, watermark)
+        project.convert("RGB").save(f"{PATH}\\Projet_{image}", format="JPEG", subsampling=0, quality=QUALITY)
+        print(f"{image} : Projet OK")
 
     if duo == True :
         if base_image.width > base_image.height:
