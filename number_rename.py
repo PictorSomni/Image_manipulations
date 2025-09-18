181e4d7373de3d1aa8726d4f4c397ef94ce5a421
Charles Dessenius <dessenius.charles@gmail.com>
2025-09-18 10:14:56 +0200

    Update number_rename.py

--- number_rename.py
+++ number_rename.py
@@ -16,8 +16,8 @@ os.chdir(PATH)
 #                         CONTENT                           #
 #############################################################
 
-EXTENSION = (".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG")
-FOLDER = [file for file in sorted(os.listdir()) if file.endswith(EXTENSION) and not file == "watermark.png"]
+EXTENSION = (".jpg", ".jpeg", ".png")
+FOLDER = [file for file in sorted(os.listdir()) if file.lower().endswith(EXTENSION) and not file == "watermark.png"]
 
 #############################################################
 #                           MAIN                            #
@@ -25,4 +25,4 @@ FOLDER = [file for file in sorted(os.listdir()) if file.endswith(EXTENSION) and 
 
 for index, file in enumerate(FOLDER):
     filename, ext = file.split(".")
-    os.rename(file, f"{index:03}.{ext}")
+    os.rename(file, f"{index + 1:03}.{ext}")
