aab72a282922ddab1d7899d7b03af701d60f0161
Charles Dessenius <dessenius.charles@gmail.com>
2026-03-06 22:04:28 +0100

    Now Conversion JPG.py converts PDF using PyMuPDF

--- Data/Conversion JPG.py
+++ Data/Conversion JPG.py
@@ -8,6 +8,7 @@ __version__ = "1.7.0"
 import os
 from pathlib import Path
 from wand.image import Image
+import fitz  # PyMuPDF
 
 #############################################################
 #                           PATH                            #
@@ -21,7 +22,7 @@ PATH = Path(__file__).resolve().parent
 selected_files_str = os.environ.get("SELECTED_FILES", "")
 selected_files_set = set(selected_files_str.split("|")) if selected_files_str else None
 
-EXTENSIONS = (".avif",".heic", ".webp", ".png", ".tiff", ".jpeg", ".bmp", ".gif", ".psd", ".svg", ".ico", ".jfif", ".jpe", ".jif", ".jfi")
+EXTENSIONS = (".avif",".heic", ".webp", ".png", ".tiff", ".jpeg", ".bmp", ".gif", ".psd", ".svg", ".ico", ".jfif", ".jpe", ".jif", ".jfi", ".pdf")
 all_files = [file for file in sorted(PATH.iterdir()) if file.is_file() and file.suffix.lower() in EXTENSIONS]
 FOLDER = [f for f in all_files if f.name in selected_files_set] if selected_files_set else all_files
 TOTAL = len(FOLDER)
@@ -47,13 +48,25 @@ else:
         folder(f"{file_extension[1:]}")
 
         try:
-            with Image(filename=str(file)) as actual_file:
-                image = actual_file.convert('jpg')
-                jpg_path = PATH / f"{file_name}.jpg"
-                image.save(filename=str(jpg_path))
+            if file_extension == ".pdf":
+                pdf_doc = fitz.open(str(file))
+                page_count = len(pdf_doc)
+                for j, page in enumerate(pdf_doc):
+                    print(f"  - Conversion de la page {j+1} sur {page_count}")
+                    pix = page.get_pixmap(dpi=300)
+                    jpg_path = PATH / f"{file_name}_{j+1:03}.jpg"
+                    pix.save(str(jpg_path))
+                pdf_doc.close()
+                print(f"  [OK] Converti: {page_count} page(s)")
+            else:
+                with Image(filename=str(file)) as actual_file:
+                    image = actual_file.convert('jpg')
+                    jpg_path = PATH / f"{file_name}.jpg"
+                    image.save(filename=str(jpg_path))
+                print(f"  [OK] Converti: {file_name}.jpg")
+
             dest_folder = PATH / f"{file_extension[1:]}"
             file.rename(dest_folder / file.name)
-            print(f"  [OK] Converti: {file_name}.jpg")
         except Exception as e:
             print(f"  [X] Erreur pour {file.name}: {e}")
 
--- Data/Renommer sequence.py
+++ Data/Renommer sequence.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 __version__ = "1.7.0"
